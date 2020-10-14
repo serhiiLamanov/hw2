4c85f2bf88aeba9caa279148c52e51611b4e9307
serhii <svlam@ukr.net>
2020-10-14 14:27:02 +0300

    Updates

---
+++ precache-manifest.0e55dd84de1ebe8207d5069ed38404e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4693f7a2fe4fce49894263fb8436cca6",
+    "url": "/hw2/index.html"
+  },
+  {
+    "revision": "ced4927ef1c11152975b",
+    "url": "/hw2/static/css/main.3f11002c.chunk.css"
+  },
+  {
+    "revision": "bbcb4291c21638bf3952",
+    "url": "/hw2/static/js/2.89d29561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw2/static/js/2.89d29561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced4927ef1c11152975b",
+    "url": "/hw2/static/js/main.caff2f05.chunk.js"
+  },
+  {
+    "revision": "acc1e5e9d080f6131f13",
+    "url": "/hw2/static/js/runtime-main.bdb0080f.js"
+  }
+]);
